c1f39869a0af5ab0c147891bc96f71a7cc02f827
thinakarankalathi <31190097+thinakarankalathi@users.noreply.github.com>
2017-09-23 19:33:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,9 +5,20 @@ var button =document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
     
+    var request = new XMLHttpRequest();
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
+            if(request.status == 200){
+                 counter = request.responseText;
+                 var span = document.getElementById('count');
+                 span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open("GET", 'http://http://thinakarankalathi.imad.hasura-app.io/counter', true);
+    request.send(null);
     
 };
